7c7a3e6d72e41d0385fb64200e6cce07568423fe
halil <halilski1979@gmail.com>
2024-10-09 12:10:06 +0300

    rwrwr

---
+++ Arrays/01-VhodElementi/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01_VhodElementi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вход на елементи от 1 ред
+            int[] num = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(string.Join(",",num));
+
+           
+        }
+    }
+}
---
+++ Arrays/02-Vhod_Elementi_Po_Red/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02_Vhod_Elementi_Po_Red
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вход на елементи ред по ред
+            int[] num = new int[5];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write($"num[{i}]=");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("===================");
+            Console.WriteLine(string.Join(" ",num));
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine($"num[{i}]={num[i]}");
+            }
+        }
+    }
+}
