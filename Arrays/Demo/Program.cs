b5882f39bef832dda84f7274ee6f16ca3dfc0ef8
halil <halilski1979@gmail.com>
2024-10-09 11:54:14 +0300

    232e32

---
+++ Arrays/Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Деклариране на масиви 
+            int[] num = new int[10];
+
+            //1. Деклариране на масиви 
+            string[] names = new string[10];
+
+            //2. Деклариране на масиви с елементи
+            int[] number = new int[] {2,4,11,2,5,6};
+            Console.WriteLine(number[1]);
+
+            //2. Деклариране на масиви с елементи
+            string [] days = new string[] { "Pon","Vt","Sr","Ch","Pet" };
+            Console.WriteLine(days[1]);
+
+        }
+    }
+}
---
+++ Arrays/Demo1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Demo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вход на елементи на масив от 1 ред
+            var num = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Console.WriteLine("====Pechat1======");
+            //Отпечатване1
+            Console.WriteLine(string.Join(" ",num));
+
+            Console.WriteLine("*******Pechat2*********");
+            //Отпечатване2  =  с цикъл For
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine($"num[{i}]={num[i]}");
+            }
+
+            //Отпечатване3 - с foreach
+            Console.WriteLine("------Pechat3------");
+            foreach (var item in num)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
