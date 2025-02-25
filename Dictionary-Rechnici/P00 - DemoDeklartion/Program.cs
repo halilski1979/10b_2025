1c951bd968aae6c337467f2eb5651d9d53a79190
Halil <halilski1979@gmail.com>
2025-02-25 13:00:18 +0200

    e23e

---
+++ Dictionary-Rechnici/P00 - DemoDeklartion/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P00___DemoDeklartion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5];
+
+            Dictionary<string, int> people = new Dictionary<string, int>();
+            var persons= new Dictionary<string, int>();
+
+            num[0] = 1;
+            num[1] = 22;
+
+            //Добавяне на елементи към речник
+            people["Ivan"] = 23;
+            people["Petar"] = 32;
+            people.Add("Asen",44);
+            people.Add("Kiril",18);
+
+            //В речник няма елементи с еднакъв ключ
+            //people.Add("Kiril", 33);
+
+            //Отпечатване
+            foreach (var item in people)
+            {
+                Console.WriteLine($"{item.Key} is  {item.Value} years old!");
+            }
+
+
+
+        }
+    }
+}
---
+++ Dictionary-Rechnici/P02 - DemoAddElements/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace P02___DemoAddElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var students=new Dictionary<string, double>();
+
+            Console.Write("Broy uchenici: ");
+            int br=int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < br; i++)
+            {
+                var school = Console.ReadLine().Split(' ').ToList();
+                var name=school[0];
+                var money=double.Parse(school[1]);
+
+                if (students.ContainsKey(name))
+                {
+                    students[name] += money;
+                }
+                else 
+                { 
+                  students.Add(name, money);
+                }
+            }
+
+            foreach (var item in students)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+
+
+        }
+    }
+}
