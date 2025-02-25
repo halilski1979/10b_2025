a69e4767d048aa3babe68a31489f7d4c520db94c
Halil <halilski1979@gmail.com>
2025-02-25 12:21:56 +0200

    fdewfwe

---
+++ LambdaFunction/P02 - Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P02___Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new List<int>() { -1, 2 ,- 3, 4, - 5, 6, 7, 8, 9 };
+            Console.WriteLine(string.Join(" ",nums));
+
+           var newNums= nums.Where(x => x % 2 == 0 && x>5).ToList();
+            Console.WriteLine(string.Join(" ", newNums));
+
+          nums.OrderBy(x=>x).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
